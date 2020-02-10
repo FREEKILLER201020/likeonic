33afbd8d8ba795866d7b5582a903fc51a7f7aad7
freekiller201020 <htc.burchenko@gmail.com>
2020-02-10 22:40:59 +0300

    bot

---
+++ bot_likeonic/create.sql
@@ -0,0 +1,119 @@
+drop table if exists public.users CASCADE;
+drop table if exists public.chat_state CASCADE;
+drop table if exists public.chat CASCADE;
+drop table if exists public.messages_history CASCADE;
+drop table if exists public.faq CASCADE;
+
+-- История сообщений пользователей (что бы можно было понять, что он пытался найти и помочь ему)
+CREATE TABLE public.messages_history (
+  -- Время сообщения
+  timemark timestamp,
+  -- id сообщения
+  id integer UNIQUE,
+  -- текст сообщения
+  message text,
+  -- из какого чата сообщение
+  chat_id integer,
+  -- от когого пользователя сообщение
+  user_id integer,
+  PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.messages_history
+    OWNER to postgres;
+
+-- Таблица пользователей, которые открыли нашего бота (данные берутся из сообщения телеграма)
+CREATE TABLE public.users (
+  -- id пользователя
+  id integer UNIQUE,
+  -- никнейм пользователя
+  username text,
+  -- имя пользователя
+  name text,
+  lang text,
+  PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.users
+    OWNER to postgres;
+
+-- Состояние чата (инициализация, поиск ответа, нужна помощь человека, вопрос решен)
+CREATE TABLE public.chat_state (
+  -- id состояния чата
+  id integer UNIQUE,
+  -- описание состояния
+  type text,
+  PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.chat_state
+    OWNER to postgres;
+
+-- Список всех чатов (!= списку пользователей, так как могут быть потенциально груповые чаты. Боту работать в них будет запрещено)
+CREATE TABLE public.chat (
+  -- id чата
+  id integer UNIQUE,
+  -- состояние чата
+  chat_state integer REFERENCES chat_state(id),
+  PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.chat_state
+    OWNER to postgres;
+
+-- Таблица вопросов и ответов
+CREATE TABLE public.faq (
+  -- id вопрос-ответа
+  id integer UNIQUE,
+  -- id родителя
+  parent integer REFERENCES faq(id),
+  chat_state integer REFERENCES chat_state(id),
+  -- вопрос
+  question_ru text,
+  question_en text,
+  question_gr text,
+  answer_ru text,
+  answer_en text,
+  answer_gr text,
+  PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+);
+
+ALTER TABLE public.faq
+    OWNER to postgres;
+
+
+insert into chat_state (id,type) values (1,'idle');
+insert into chat_state (id,type) values (2,'general');
+insert into chat_state (id,type) values (3,'web_app');
+insert into chat_state (id,type) values (4,'app');
+insert into chat_state (id,type) values (5,'staff_app');
+insert into chat_state (id,type) values (6,'help');
+
+insert into faq (id,chat_state,question_ru,answer_ru) values (0,1,'Добро пожаловать в чат-бота Likeonic!','');
+
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (1,0,1,'Общие вопросы','');
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (2,0,1,'Вопросы по WEB приложению','');
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (3,0,1,'Вопросы по мобильному приложению','');
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (4,0,1,'Вопросы по приложению для персонала','');
+
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (5,1,2,'Что такое Лайкоин','Лайкоин (LCO) – цифровая валюта лояльности, которую вы автоматически получаете в качестве кэшбэка за покупки и за выполнение заданий, а так же за участие в реферальной программе «Пригласи друга». Лайкоины поступают на ваш персональный аккаунту, где надёжно хранятся в системе без истечения срока действия и могут быть использованы при следующей оплате в партнерском магазине или продолжать накапливаться на более крупные покупки.\nКурс Лайкоина всегда стабилен: 1 LCO = 1 EUR ');
+insert into faq (id,parent,chat_state,question_ru,answer_ru) values (6,1,2,'Как получить Лайкоины','Лайкоин (LCO) – цифровая валюта лояльности, которую вы автоматически получаете в качестве кэшбэка за покупки и за выполнение заданий, а так же за участие в реферальной программе «Пригласи друга». Лайкоины поступают на ваш персональный аккаунту, где надёжно хранятся в системе без истечения срока действия и могут быть использованы при следующей оплате в партнерском магазине или продолжать накапливаться на более крупные покупки.\nКурс Лайкоина всегда стабилен: 1 LCO = 1 EUR ');
+
+
+
+
