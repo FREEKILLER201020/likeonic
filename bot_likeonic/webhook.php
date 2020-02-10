82e33a38c8de0c302f57e34091f60da06b2b351e
freekiller201020 <htc.burchenko@gmail.com>
2020-02-11 00:44:50 +0300

    Update webhook.php

--- bot_likeonic/webhook.php
+++ bot_likeonic/webhook.php
@@ -15,6 +15,11 @@ $token = $config['token'];
 // Инициализация бота
 $bot = new \TelegramBot\Api\Client($token);
 
+// команда для start
+$bot->command('start', function ($message) use ($bot) {
+	Start($message, $bot);
+});
+
 $bot->on(function ($Update) use ($bot) {
 
 	$message = $Update->getMessage();
@@ -27,11 +32,6 @@ $bot->on(function ($Update) use ($bot) {
 	$bot->sendMessage($message->getChat()->getId(), $answer);
 }, function ($message) use ($name) {
 	return true; // когда тут true - команда проходит
-});
-
-// команда для start
-$bot->command('start', function ($message) use ($bot) {
-	Start($message, $bot);
 });
 
 $bot->run();
