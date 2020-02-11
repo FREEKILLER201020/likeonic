e66288241b165b79163363fa7a0fd393d91f4935
freekiller201020 <htc.burchenko@gmail.com>
2020-02-11 18:09:56 +0300

    Update webhook.php

--- bot_likeonic/webhook.php
+++ bot_likeonic/webhook.php
@@ -36,9 +36,9 @@ $bot->on(function ($Update) use ($bot) {
 	}
 	SaveMessage($mtext, $chat_id, $user_id);
 	$answer = AskCurrentQuestion($user_id);
-	$keyboard = AskNextQuestion($user_id);
+	$answer = AskNextQuestion($user_id);
 	// $keyboard = AskCurrentAnswers($user_id);
-	// $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardHide();
+	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardHide();
 	// $bot->sendMessage($message->getChat()->getId(), $answer);
 	$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
 }, function ($message) use ($name) {
@@ -129,9 +129,9 @@ function AskNextQuestion($user) {
 	while ($data = pg_fetch_object($result)) {
 		array_push($answers, ["text" => $data->question]);
 	}
-	// $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[$answers]], true, true);
-	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardHide();
-	// return var_export($answers, true);
-	return $keyboard;
+	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[]], true, true);
+	// $keyboard = new \TelegramBot\Api\Types\ReplyKeyboardHide();
+	return var_export($keyboard, true);
+	// return $keyboard;
 }
 ?>
