55a5f576a06451dcf52e7d5d497aaa638a0f12a5
freekiller201020 <htc.burchenko@gmail.com>
2020-02-11 17:36:30 +0300

    Update webhook.php

--- bot_likeonic/webhook.php
+++ bot_likeonic/webhook.php
@@ -36,7 +36,7 @@ $bot->on(function ($Update) use ($bot) {
 	}
 	SaveMessage($mtext, $chat_id, $user_id);
 	$answer = AskCurrentQuestion($user_id);
-	$keyboard=AskCurrentAnswers($user_id);
+	// $keyboard=AskCurrentAnswers($user_id);
 	// $bot->sendMessage($message->getChat()->getId(), $answer);
 	$bot->sendMessage($message->getChat()->getId(), $answer, false, null, null, $keyboard);
 }, function ($message) use ($name) {
@@ -105,18 +105,5 @@ function AskCurrentQuestion($user) {
 	}
 	return $question;
 	// return $query;
-}
-
-function AskCurrentAnswers($user) {
-	$user = intval($user);
-	$query = "Select answer from answers where question=(select current_question from chats where user_id=$user);";
-	$result = pg_query($query);
-	$answers = array();
-	while ($data = pg_fetch_object($result)) {
-		array_push($answers, "text"=>$data->answer);
-	}
-	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[$answers]], true, true);
-	return $keyboard;
-	// return $query;
-}
+}; // function AskCurrentAnswers($user) {; // 	$user = intval($user);; // 	$query = "Select answer from answers where question=(select current_question from chats where user_id=$user);";; // 	$result = pg_query($query);; // 	$answers = array();; // 	while ($data = pg_fetch_object($result)) {; // 		array_push($answers, "text"=>$data->answer);; // 	}; // 	$keyboard = new \TelegramBot\Api\Types\ReplyKeyboardMarkup([[$answers]], true, true);; // 	return $keyboard;; // 	// return $query;; // }
 ?>
